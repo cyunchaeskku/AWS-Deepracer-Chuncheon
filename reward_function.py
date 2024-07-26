ca00f689d62b8490cce2a118466d005a6416bb64
cyunchaeskku <65405716+cyunchaeskku@users.noreply.github.com>
2024-07-26 07:29:33 +0000

    reward function edited Added stay inside borders reward function

--- reward_function.py
+++ reward_function.py
@@ -1,25 +1,85 @@
 def reward_function(params):
-    '''
-    Example of rewarding the agent to follow center line
-    '''
-    
-    # Read input parameters
-    track_width = params['track_width']
+
+    import math
+
+    on_track = params['all_wheels_on_track']
     distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    steering = abs(params['steering_angle']) # Only need the absolute steering angle for calculations
+    speed = params['speed']
+    waypoints = params['waypoints']
+    closest_waypoints = params['closest_waypoints'] 
+    heading = params['heading']
+    all_wheels_on_track = params['all_wheels_on_track']
+
+    reward = math.exp(-6 * distance_from_center)
+
+    def on_track_reward(current_reward, on_track):
+        if not on_track:
+            current_reward = 1e-3
+        else:
+            current_reward = 1e2
+        return current_reward
     
-    # Calculate 3 markers that are at varying distances away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
+    def distance_from_center_reward(current_reward, track_width, distance_from_center):
     
-    # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-        reward = 1.0
-    elif distance_from_center <= marker_2:
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-        reward = 0.1
-    else:
-        reward = 1e-3  # likely crashed/ close to off track
+        # Calculate 3 markers that are at varying distances away from the center line
+        marker_1 = 0.1 * track_width
+        marker_2 = 0.25 * track_width
+        marker_3 = 0.5 * track_width
+        
+        # Give higher reward if the car is closer to center line and vice versa
+        if distance_from_center <= marker_1:
+            current_reward *= 1.2
+        elif distance_from_center <= marker_2:
+            current_reward *= 0.8
+        elif distance_from_center <= marker_3:
+            current_reward *= 0.5
+        else:
+            current_reward = 1e-3  # likely crashed/ close to off track
+        
+        return float(current_reward)
+
+    def throttle_reward(current_reward, speed, steering):
+        if speed > 2.5 - (0.4 * abs(steering)):
+            current_reward *= 0.8
+        return current_reward
+
+    def steering_reward(current_reward, steering):
+        if abs(steering) > 25:
+            current_reward *= 0.8
+        return current_reward
+
+    def straight_line_reward(current_reward, steering, speed):
+        # Positive reward if the car is in a straight line going fast
+        if abs(steering) < 0.1 and speed > 3:
+            current_reward *= 1.2
+        return current_reward
     
+    def speed_reward(current_reward, speed):
+        if speed>2:
+            current_reward *= 1.1
+        return current_reward
+
+    def stay_inside_border_reward(current_reward, all_wheels_on_track, distance_from_center, track_width):
+        
+        # Give a very low reward by default
+        reward = 1e-3
+
+        # Give a high reward if no wheels go off the track and 
+        # the car is somewhere in between the track borders 
+        if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
+            reward = 1.0
+
+        # Always return a float value
+        return reward
+        
+
+    reward = on_track_reward(reward, on_track)
+    reward = distance_from_center_reward(reward, track_width, distance_from_center)
+    reward = throttle_reward(reward, speed, steering)
+    reward = steering_reward(reward, steering)
+    reward = straight_line_reward(reward, steering, speed)
+    reward = speed_reward(reward, speed)
+
     return float(reward)
